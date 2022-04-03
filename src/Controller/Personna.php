08206af51e3230171532c8fb73d171e6046bec19
viduc <tristan.fleury@univ-grenoble-alpes.fr>
2022-04-03 21:02:47 +0200

    code climate

--- src/Controller/Personna.php
+++ src/Controller/Personna.php
@@ -9,12 +9,14 @@ declare(strict_types=1);
 
 namespace Viduc\Personna\Controller;
 
+use SebastianBergmann\CodeCoverage\Report\Xml\Node;
 use Viduc\Personna\Exceptions\PersonnaRepositoryException;
 use Viduc\Personna\Exceptions\PersonnaRequetesException;
 use Viduc\Personna\File\File;
 use Viduc\Personna\Interfaces\Controller\UseCaseInterface;
 use Viduc\Personna\Interfaces\File\FileInterface;
 use Viduc\Personna\Interfaces\Presenters\PresenterInterface;
+use Viduc\Personna\Interfaces\Reponses\ReponseInterface;
 use Viduc\Personna\Interfaces\Requetes\RequeteInterface;
 use Viduc\Personna\Model\ErreurModel;
 use Viduc\Personna\Reponses\ReponseCreate;
@@ -26,6 +28,10 @@ class Personna implements UseCaseInterface
      * @var PersonnaRepository
      */
     private PersonnaRepository $repository;
+
+    private RequeteInterface $requete;
+
+    private ReponseInterface $reponse;
 
     public function __construct(string $path)
     {
@@ -41,60 +47,52 @@ class Personna implements UseCaseInterface
         RequeteInterface $requete,
         PresenterInterface $presenter
     ): PresenterInterface {
+        $this->requete = $requete;
         switch ($requete->getAction()) {
             case 'create':
-                $reponse = new ReponseCreate();
-                try {
-                    $reponse->setPersonnaModel($this->repository->create(
-                        $requete->getParam('personna')
-                    ));
-                } catch (PersonnaRepositoryException | PersonnaRequetesException $ex) {
-                    $reponse->setErreur(
-                        new ErreurModel($ex->getCode(), $ex->getMessage())
-                    );
-                }
-                $presenter->presente($reponse);
+                $this->reponse = new ReponseCreate();
+                $this->reponsePersonna('create', 'personna');
                 break;
             case 'read':
-                $reponse = new ReponseCreate();
-                try {
-                    $reponse->setPersonnaModel(
-                        $this->repository->read($requete->getParam('id'))
-                    );
-                } catch (PersonnaRepositoryException | PersonnaRequetesException $ex) {
-                    $reponse->setErreur(
-                        new ErreurModel($ex->getCode(), $ex->getMessage())
-                    );
-                }
-                $presenter->presente($reponse);
+                $this->reponse = new ReponseCreate();
+                $this->reponsePersonna('read', 'id');
                 break;
             case 'update':
-                $reponse = new ReponseCreate();
-                try {
-                    $reponse->setPersonnaModel(
-                        $this->repository->update($requete->getParam('personna'))
-                    );
-                } catch (PersonnaRepositoryException | PersonnaRequetesException $ex) {
-                    $reponse->setErreur(
-                        new ErreurModel($ex->getCode(), $ex->getMessage())
-                    );
-                }
-                $presenter->presente($reponse);
+                $this->reponse = new ReponseCreate();
+                $this->reponsePersonna('update', 'personna');
                 break;
             case 'delete':
-                $reponse = new ReponseCreate();
+                $this->reponse = new ReponseCreate();
                 try {
                     $this->repository->delete($requete->getParam('personna'));
                 } catch (PersonnaRepositoryException | PersonnaRequetesException $ex) {
-                    $reponse->setErreur(
+                    $this->reponse->setErreur(
                         new ErreurModel($ex->getCode(), $ex->getMessage())
                     );
                 }
-                $presenter->presente($reponse);
                 break;
         }
+        $presenter->presente($this->reponse);
 
         return $presenter;
+    }
+
+    /**
+     * @param string $action
+     * @param string $param
+     * @return void
+     */
+    private function reponsePersonna(string $action, string $param): void
+    {
+        try {
+            $this->reponse->setPersonnaModel($this->repository->$action(
+                $this->requete->getParam($param)
+            ));
+        } catch (PersonnaRepositoryException | PersonnaRequetesException $ex) {
+            $this->reponse->setErreur(
+                new ErreurModel($ex->getCode(), $ex->getMessage())
+            );
+        }
     }
 
     /**
